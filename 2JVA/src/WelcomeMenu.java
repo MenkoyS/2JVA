3f928a7c3fc279b5f971c177351b34c6f8f6f70e
Yann Hobbe <yann.hobbe@supinfo.com>
2025-01-15 14:11:14 +0100

    Setup Login / Register Menu

---
+++ 2JVA/src/WelcomeMenu.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        System.out.println("Would you like to login or to register ?");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+
+        System.out.println("Enter your choice: ");
+
+        // if the user enters 1 then the program will print "Login"
+        // if the user enters 2 then the program will print "Register"
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println("Login");
+        } else if (choice == 2) {
+            System.out.println("Register");
+        } else {
+            System.out.println("Invalid choice");
+        }
+    }
+}
