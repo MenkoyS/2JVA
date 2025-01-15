738b030c06df8a771d14115e23822089181362bd
Yann Hobbe <yann.hobbe@supinfo.com>
2025-01-15 15:36:51 +0100

    Database Connect setup

---
+++ 2JVA/src/DBManager.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+    private final String url;
+    private final String username;
+    private final String password;
+    private Connection connection;
+
+    public DB(String url, String username, String password) {
+        this.url = "jdbc:mysql://localhost:3306/Ikea";
+        this.username = "root";
+        this.password = "root";
+    }
+
+    public void connect() {
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Database connected successfully!");
+        } catch (SQLException e) {
+            System.out.println("Database connection failed: " + e.getMessage());
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Database connection closed.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to close the connection: " + e.getMessage());
+        }
+    }
+}
