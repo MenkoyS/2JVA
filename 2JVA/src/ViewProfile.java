c5545884433cc302797615ba300281941601c98e
Yann Hobbe <yann.hobbe@supinfo.com>
2025-01-16 10:09:54 +0100

    ViewProfile Setup

---
+++ 2JVA/src/ViewProfile.java
@@ -0,0 +1,11 @@
+public class ViewProfile {
+    public static void main(String[] args) {
+        System.out.println("Profile: ");
+
+        // Show Pseudo, Email, Store
+
+        System.out.println("Pseudo: ");
+        System.out.println("Email: ");
+        System.out.println("Store Affiliation: ");
+    }
+}
