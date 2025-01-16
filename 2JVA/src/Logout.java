166c85d3a442aa1fc0168f8511a6c6bb86cf8497
Yann Hobbe <yann.hobbe@supinfo.com>
2025-01-16 10:22:22 +0100

    Logout Setup

---
+++ 2JVA/src/Logout.java
@@ -0,0 +1,7 @@
+public class Logout {
+    public static void main(String[] args) {
+        System.out.println("You are getting logged off !");
+
+        // Sends back to login/register page
+    }
+}
