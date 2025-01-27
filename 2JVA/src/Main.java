c2613523cafe8b204f530de89dd4458ebdc2efda
Yann Hobbe <yann.hobbe@supinfo.com>
2025-01-27 13:50:44 +0100

    ManageUsers complete.

--- 2JVA/src/Main.java
+++ 2JVA/src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        ManageUsers.main(args);
+        Scanner scanner = new Scanner(System.in);
+        ManageUsers.main(scanner);
     }
 }
